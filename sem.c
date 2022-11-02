20f35d95e080daa71c7946efe64685f5ff1be527
ekbostan <114015851+ekbostan@users.noreply.github.com>
2022-11-01 21:15:34 -0700

    Update sem.c

--- sem.c
+++ sem.c
@@ -6,8 +6,23 @@
 #include "private.h"
 
 struct semaphore {
-	int internal_count;
+	size_t internal_count;
+	queue_t sem_queue;
 };
+ int test_and_set(int mem)
+{
+    int oldval = mem;
+    mem = 1;
+    return oldval;
+}
+void spinlock_lock(int lock)
+{
+    while (test_and_set(lock) == 1);
+}
+void spinlock_unlock(int lock)
+{
+    lock = 0;
+}
 
 sem_t sem_create(size_t count)
 {
@@ -17,18 +32,19 @@ sem_t sem_create(size_t count)
 	}
 	else{
 	sem_lock->internal_count = count;
+	sem_lock->sem_queue = queue_create();
 	return sem_lock;
 	}
 }
 
 int sem_destroy(sem_t sem)
 {
-	if(sem == NULL || sem->internal_count == 0){
+	if(sem == NULL || sem->internal_count > 0){
 	
 	return -1;
 	}
 	else{
-	
+	queue_destroy(sem->sem_queue);
 	free(sem);
 	return 0;
 	
@@ -42,23 +58,36 @@ int sem_down(sem_t sem)
 	}
 
 
-	spinlock_lock(sem->lock);
+	spinlock_lock(sem->internal_count);
 	while(sem ->internal_count == 0){
-	
+	pthread_t id = currentThread->TID;
+	queue_enqueue(sem->sem_queue, (void*)id);
+	uthread_block();
 	
 	
 	}
 	sem->internal_count--;
-	}
+	spinlock_unlock(sem->internal_count);
+	
 	return 0;
-}
+	}
 
 int sem_up(sem_t sem)
 {
 	if(sem == NULL){
         return -1;
         }
+
+	spinlock_lock(sem->internal_count);
 	sem->internal_count++;
+	if(sem->internal_count >0){
+	pthread_t tid,
+	queue_dequeue(sem->sem_queue,(void*)tid);
+	uthread_unblock(tid);
+	
+	}
+	spinlock_unlock(sem->internal_count);
+
 	return 0;
 
 }
